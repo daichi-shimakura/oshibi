4b7f85c7148128276df931c18c18975eed8a156e
daichi-shimakura <dshimakura@gmail.com>
2026-04-02 19:10:54 +0900

    feat: add unit tests for  LiveService

---
+++ src/test/java/com/oshibi/oshibi/service/LiveServiceTest.java
@@ -0,0 +1,52 @@
+package com.oshibi.oshibi.service;
+
+import com.oshibi.oshibi.domain.entity.LivePerformer;
+import com.oshibi.oshibi.dto.ComedianLiveRequestDto;
+import com.oshibi.oshibi.repository.LivePerformerRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class LiveServiceTest {
+
+    @Mock
+    private LivePerformerRepository livePerformerRepository;
+
+    @InjectMocks
+    private LiveService liveService;
+
+    @Test
+    void LivePerformerテーブルが見つからなかった場合例外が投げられる(){
+        long liveId = 1;
+        long accountId = 2;
+        var dto = new ComedianLiveRequestDto();
+
+        when(livePerformerRepository.findByLive_LiveIdAndComedian_AccountId(liveId,accountId))
+                .thenReturn(Optional.empty());
+
+        assertThrows(NoSuchElementException.class, () -> liveService.saveComedianLive(liveId, accountId, dto));
+    }
+    @Test
+    void 正常登録でlivePerformerRepositoryのsaveが呼ばれる(){
+        long liveId = 1;
+        long accountId = 2;
+        var dto = new ComedianLiveRequestDto();
+        dto.setStatus("TENTATIVE");
+
+        when(livePerformerRepository.findByLive_LiveIdAndComedian_AccountId(liveId,accountId))
+                .thenReturn(Optional.of(new LivePerformer()));
+
+        liveService.saveComedianLive(liveId,accountId,dto);
+
+        verify(livePerformerRepository, times(1)).save(any(LivePerformer.class));
+    }
+}
